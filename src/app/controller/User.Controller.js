a545a3d84afe6cbfe3a40b84bcad651b700d7d3f
KDOE-117 <jdn2037@gmail.com>
2023-03-13 12:19:05 -0500

    Basic validation

--- src/app/controller/User.Controller.js
+++ src/app/controller/User.Controller.js
@@ -4,23 +4,26 @@ const User = require("../../models/user.model.js");
 class UserController {
     static createUser = async (req, res) => {
         try {
-            console.log("preBody", req.body);
             const { nombre, contraseña, email } = req.body;
             const dataUser = {
                 nombre: nombre,
                 contraseña: contraseña,
                 email: email
             };
-            console.warn("dataUser", dataUser);
-            //VALIDACIONES!!
-            //Verificar que lo que se está enviando está vacio
-            if (Object.entries(dataUser.nombre || dataUser.contraseña || dataUser.email).length === 0) {
-                res.redirect('/formulario')
+            //Validaciones
+            if (Object.entries(dataUser.nombre && dataUser.contraseña && dataUser.email).length === 0) {
+                res.send({
+                    status: 400,
+                    message: 'No se aceptan vacios!'
+                });
             } else {
-                new User(req.body);
-                console.log("dataUser", dataUser);
-                const exec = await dataUser.create()
-                res.redirect('/formulario')
+                const usuario = new User(req.body);
+                const ejecutar = await usuario.create()
+                if (ejecutar.affectedRows > 0) {
+                    res.status(202).json({
+                        message: "Area Created Successfully"
+                    })
+                }
             }
         } catch (error) {
             return res.send({
